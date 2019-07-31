4fe0c7f8eba45347fb2923ab1bf29b1a87be97fd
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2019-07-31 14:14:19 +0200

    created new component for music

---
+++ src/app/music/music.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-music',
+  templateUrl: './music.component.html',
+  styleUrls: ['./music.component.scss']
+})
+export class MusicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("cool")
+  }
+
+}
