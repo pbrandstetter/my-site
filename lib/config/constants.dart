590f090484109207b202707d3b5ffa27e68e4df3
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2021-06-18 09:48:31 +0200

    initial ng

--- lib/config/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const PROFILE_GITHUB = 'https://github.com/pbrandstetter';
-  static const PROFILE_TWITTER = 'https://twitter.com/phips_b';
-  static const PROFILE_INSTAGRAM = 'https://instagram.com/phips.b';
-  static const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/philipp-brandstetter-25869a199/';
-}
