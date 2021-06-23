dcfddd9f7eaf1745520bac14dfe3cb5b04bf3bc2
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2021-06-23 15:18:23 +0200

    created component experience

--- philipp/src/app/experience/experience.component.ts
+++ philipp/src/app/experience/experience.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import ExperienceJson from '../../assets/experience.json';
+
+class Experience {
+  title: string;
+  desc: string;
+  time: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
+  experiences: Experience[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.experiences = ExperienceJson.experiences;
   }
-
 }
